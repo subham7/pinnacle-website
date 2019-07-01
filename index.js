1e4cf690f9581f0903177cd34d82f185f4d028d3
subham <subham.sengupta7@gmail.com>
2019-07-01 20:53:43 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const compression = require("compression")
+
+var app = express()
+var port = process.env.PORT || 8088
+
+app.use(compression())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", function(req, resp) {
+  resp.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.get("/:page", function(req, resp) {
+  resp.sendFile(path.join(__dirname, "public", `${req.params.page}`))
+})
+
+app.listen(port, function() {
+  console.log(`Server started at ${port}`)
+})
